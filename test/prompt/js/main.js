4f697a3e8e2ae40099e68951c7be9999d3189973
DevSaulSneider <110130146+DevSaulSneider@users.noreply.github.com>
2023-01-20 14:06:35 -0500

    prompt y flex3

---
+++ test/prompt/js/main.js
@@ -0,0 +1,85 @@
+//Variable
+// let edad = 23;
+// edad = 20;
+
+// let nombre;
+// nombre = "Saul";
+
+// let Distrito = "Los olivos";
+// Distrito = "Pte Piedra";
+
+// alert (edad);
+
+// alert(nombre);
+
+// alert (Distrito);
+
+// console.log(edad);
+
+
+
+// PROMPT
+
+// let mensaje = "Hola como te llamas?"
+// let guardar = prompt (mensaje);
+
+// let texto = "Tu nombre es:";
+
+// alert("Tu nombre es:" + " " + guardar);
+
+
+// let numero1 = 3;
+// let numero2 = 5;
+
+// let resultado = numero1 + numero2;
+
+// alert (resultado);
+
+
+// numero1 = prompt("Ingrese numero1");
+// let numero1_number= parseInt(numero1);
+
+// numero2 = prompt("Ingrese numero2:");
+// let numero2_number= parseInt(numero2);
+
+// let Resultado = numero1_number + numero2_number;
+
+// alert ("tu resultado es:" + " " + Resultado);
+
+
+// let edad = prompt("Que edad tienes?");
+
+// if (edad >= 18) {
+//     alert("Eres mayor de edad");
+// } else {
+//     alert("No puedes ingresar");
+// }
+
+
+
+// 1.- En un evento para aprender desarrollo web, solo se acepta a chicas y chicos que tenga un promedio minimo de 18, se necesita preguntar el promedio de los alumnos para comprobar si cumple lo necesario.
+
+let promedio = prompt("¿Cual es tu promedio?");
+
+let Promedio = parseInt(promedio);
+
+if (Promedio >= 18) {
+    alert("Cumple requisitos");
+} else {
+    alert("No cumple requisitos");
+}
+
+
+
+
+
+
+// 2.- Los mismo alumnos tienen que sumar entre todos 90 para que puedan obtener una beca y puedan estudiar gratis, son 5 alumnos en total, determinar si cumplen los requisitos para la beca
+
+
+
+
+
+// 3.- todos los alumnos salieron a desayunar y tiene en total 130 para gastar en comida, hacen 3 compras distintas y necesitan saber cuanto les queda de dinero
+
+
